5a8d85ae8aedf3ac7a2bf4c315a2635f008831ab
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-07-21 17:24:59 +0800

    Week 2 optional `rwc` finished

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
 use std::process;
 
 fn main() {
@@ -8,5 +10,17 @@ fn main() {
         process::exit(1);
     }
     let filename = &args[1];
-    // Your code here :)
+    let file = File::open(filename).expect("Error opening file");
+    let buf_reader = io::BufReader::new(file);
+    let lines = buf_reader.lines();
+    let mut n_lines: usize = 0;
+    let mut n_words: usize = 0;
+    let mut n_chars: usize = 0;
+    for line in lines {
+        let line_content = line.unwrap();
+        n_lines += 1;
+        n_words += line_content.split_whitespace().count();
+        n_chars += line_content.chars().count();
+    }
+    println!("{} {} {}", n_lines, n_words, n_chars);
 }
